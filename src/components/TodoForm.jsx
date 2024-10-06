db4d6036dcc216ecb62bddd3c2f27e87befe1c98
ia.sazhinov@tensor.ru <ia.sazhinov@tensor.ru>
2024-10-06 11:32:20 +0300

    Проверка на заполненное поле с делом при добавлени

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -8,21 +8,26 @@ import MyInput from "./UI/Input/MyInput";
 const TodoForm = ({create}) => {
 
     const [todo, setTodo] = useState({text: '', date: useDateNow()})
+    const [description, setDescription] = useState('Описание задачи')
 
     // создаю новую задачу
     const addNewTodo = (e) => {
     e.preventDefault()
-    const newTodo = {
-        ...todo, id: Date.now(), completed: false
-    }
-    create(newTodo)
-    setTodo({text: '', date: useDateNow()})
-  }
+    if (todo.text != '') {
+        const newTodo = {
+            ...todo, id: Date.now(), completed: false
+        }
+        create(newTodo)
+        setTodo({text: '', date: useDateNow()})
+        setDescription('Описание задачи')
+    } else {
+        setDescription('Поле дело обязательно для заполнения')
+    }}
 
     return (
         <form className="todoForm">
             <MyDate  value={todo.date} onChange={e => setTodo({...todo, date: e.target.value})}/>
-            <MyInput value={todo.text} onChange={e => setTodo({...todo, text: e.target.value})}>Описание задачи</MyInput>
+            <MyInput value={todo.text} onChange={e => setTodo({...todo, text: e.target.value})}>{description}</MyInput>
             <MyButton onClick={addNewTodo}>Добавить</MyButton>
         </form>
     );
